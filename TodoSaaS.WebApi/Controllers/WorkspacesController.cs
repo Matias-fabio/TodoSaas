d6575f0945398715a6c0f80686e2a49a471dad8f
Matias <matias24_05.96@hotmail.com>
2026-07-20 23:31:57 -0300

    fature:save old changes

--- TodoSaaS.WebApi/Controllers/WorkspacesController.cs
+++ TodoSaaS.WebApi/Controllers/WorkspacesController.cs
@@ -2,11 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoSaaS.Application.Workspaces.Commands.CreateWorkspace;
 
 namespace TodoSaaS.WebApi.Controllers
 {
-    public class WorkspacesController
+    public class WorkspacesController : ApiControllerBase
     {
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Create(CreateWorkspaceCommand command)
+        {
+            var workspaceId = await Mediator.Send(command);
+            return Ok(workspaceId);
+        }
         
     }
 }
